eee75eca85a42cba9e4c4eced1706eac1829326b
Nicolas Schejtman <nschejtman93@gmail.com>
2021-06-18 13:35:04 -0300

    Refactor querying code

--- src/main/scala/WebApisExample.scala
+++ src/main/scala/WebApisExample.scala
@@ -64,32 +64,52 @@ object WebApisExample {
       ontology       <- Rdf.IO.read("src/main/resources/web-apis/ontologies/Documentation.ontology.ttl", lang = "TTL")
       inferenceModel <- Rdf.Inference.default(ontology, rdf)
       _              <- Rdf.IO.write(inferenceModel, fileUrl.noProtocol.withExtension(".inf.jsonld"), "JSON-LD", resolved.id)
-      _              <- runQueriesOn(inferenceModel)
+      _              <- runQueriesOn(inferenceModel, fileUrl)
     } yield {
       rdf
     }
   }
 
-  private def runQueriesOn(model: Model): Future[Unit] = {
-    val rootDir = "src/main/resources/web-apis/queries"
+  private def runQueriesOn(model: Model, modelName: String): Future[Unit] = {
+    println(Console.BLUE)
+    println(s"Querying model: $modelName")
+    println(Console.RESET)
+    val queriesDir = "src/main/resources/web-apis/queries"
     for {
-      isWellDocumented <- Rdf.Query.ask(model, s"$rootDir/is-fully-documented.sparql")
-      _                <- println(s"Fully documented: $isWellDocumented").wrapFuture
-      coverageResult   <- Rdf.Query.select(model, s"$rootDir/documentation-coverage.sparql")
-      _ <- Future.successful {
-        val solution        = coverageResult.next()
-        val total           = solution.getLiteral("total").getDouble
-        val totalDocumented = solution.getLiteral("totalDocumented").getDouble
-        val coverage        = totalDocumented / total
-        println(s"Documentation coverage: ${coverage.asPercentage}")
-      }
-      _         <- Rdf.IO.print(coverageResult)
-      _         <- println("Missing description for: ").wrapFuture
-      resultSet <- Rdf.Query.select(model, s"$rootDir/list-undocumented.sparql")
-      _         <- Rdf.IO.print(resultSet)
+      isFullyDocumented <- queryIsFullyDocumented(model, queriesDir)
+      _                 <- queryCoverage(model, queriesDir)
+      _                 <- if (!isFullyDocumented) queryUndocumentedNodes(model, queriesDir) else Future.unit
     } yield {
       Unit
     }
   }
 
+  private def queryIsFullyDocumented(model: Model, queriesDir: String) = {
+    for {
+      isFullyDocumented <- Rdf.Query.ask(model, s"$queriesDir/is-fully-documented.sparql")
+    } yield {
+      println(s"Fully documented: $isFullyDocumented")
+      isFullyDocumented
+    }
+  }
+  private def queryCoverage(model: Model, queriesDir: String) = {
+    for {
+      coverageResult <- Rdf.Query.select(model, s"$queriesDir/documentation-coverage.sparql")
+    } yield {
+      val solution        = coverageResult.next()
+      val total           = solution.getLiteral("total").getDouble
+      val totalDocumented = solution.getLiteral("totalDocumented").getDouble
+      val coverage        = totalDocumented / total
+      println(s"Documentation coverage: ${coverage.asPercentage}")
+    }
+  }
+  private def queryUndocumentedNodes(model: Model, queriesDir: String) = {
+    for {
+      _         <- println("Missing description for: ").wrapFuture
+      resultSet <- Rdf.Query.select(model, s"$queriesDir/list-undocumented.sparql")
+      _         <- Rdf.IO.print(resultSet)
+    } yield {
+      Unit
+    }
+  }
 }
