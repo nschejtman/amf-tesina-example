cab88e8804b0345eedabf6331812b33dcd6c4999
Nicolas Schejtman <nschejtman93@gmail.com>
2021-08-16 16:15:08 -0300

    Enable ConstructQueryTransformer to be additive

--- src/main/scala/pipeline/transformer/ConstructQueryTransformer.scala
+++ src/main/scala/pipeline/transformer/ConstructQueryTransformer.scala
@@ -3,6 +3,10 @@ package pipeline.transformer
 import org.apache.jena.rdf.model.Model
 import utils.jena.Query
 
-case class ConstructQueryTransformer(queryUrl: String) extends Transformer {
-  override def transform(model: Model): Model = Query.construct(model, queryUrl)
+case class ConstructQueryTransformer(queryUrl: String, additive: Boolean) extends Transformer {
+  override def transform(model: Model): Model = {
+    val result = Query.construct(model, queryUrl)
+    if (additive) model.union(result)
+    else result
+  }
 }
